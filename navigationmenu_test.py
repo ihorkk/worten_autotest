652e5fc486a4906445131486c72ceb1bd667faf9
ihorkk <tonym0ntana@protonmail.com>
2022-09-29 11:24:11 +0100

    worten_autotest

---
+++ navigationmenu_test.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# Disable annoying notifications in browser
+option = Options()
+option.add_argument('--disable-notifications')
+
+s = Service('D:\chromedriver_win32\chromedriver.exe')
+driver = webdriver.Chrome(service=s, options=option)
+driver.maximize_window()
+url = "https://www.worten.pt"
+
+# Go to site
+driver.get(url)
+
+# Accept cookies
+acceptcookies = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((
+        By.CSS_SELECTOR, "button[class*=w-button-primary]"))).click()
+
+# Navigation menu test
+actualresult_list = ['Diretório de Categorias',
+                     'Promoções',
+                     'Novidades Tecnológicas',
+                     'Serviços Worten Resolve',
+                     'Prestadores de Serviços e Profissionais Zaask',
+                     'Lojas',
+                     'Outlet',
+                     'Worten Business | Worten para Empresas e Negócios']
+actualresult_check = []
+for indx in range(8):
+    button1 = WebDriverWait(driver, 10).until(
+        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "li[class*=nav-item-first]")))
+    button1[indx].click()
+    actualresult = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "[class*=w-breadcrumbs__text-big-name]"))).text
+    actualresult_check.append(actualresult)
+assert actualresult_list == actualresult_check, f"Names do not match \nActualResult is: {actualresult_check}"
+driver.quit()
